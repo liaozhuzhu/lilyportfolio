eee2c6ecaaf442044399252c66abe2e0ee7f6654
Liao Zhu <liao.zhu.cs@gmail.com>
2023-07-04 12:42:32 -0500

    aklsdjf;laks

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -4,13 +4,13 @@
             <NuxtLink to="/"><img src="~/assets/imgs/LilyDosedel.svg" alt="logo" class="w-32 m-8 ml-0"/></NuxtLink>
         </div>
         <ul class="flex-col leading-10 text-xl">
-            <li><NuxtLink to="/">Home</NuxtLink></li>
-            <li><div>Search</div></li>
-            <li><NuxtLink to="/explore">Explore</NuxtLink></li>
-            <li><NuxtLink to="/reels">Reels</NuxtLink></li>
-            <li><NuxtLink to="/recommendations">Recommendations</NuxtLink></li>
-            <li><NuxtLink to="/contact">Contact</NuxtLink></li>
-            <li><NuxtLink to="/profile">Profile</NuxtLink></li>
+            <li><NuxtLink to="/"><font-awesome-icon icon="fa-solid fa-house" class="mr-2"/>Home</NuxtLink></li>
+            <li><div><font-awesome-icon icon="fa-solid fa-search" class="mr-2"/>Search</div></li>
+            <li><NuxtLink to="/explore"><font-awesome-icon icon="fa-solid fa-compass" class="mr-2"/>Explore</NuxtLink></li>
+            <li><NuxtLink to="/reels"><font-awesome-icon icon="fa-solid fa-clapperboard" class="mr-2" />Reels</NuxtLink></li>
+            <li><NuxtLink to="/recommendations"><font-awesome-icon icon="fa-solid fa-comment-dots" class="mr-2"/>Recommendations</NuxtLink></li>
+            <li><NuxtLink to="/contact"><font-awesome-icon icon="fa-solid fa-square-plus" class="mr-2"/>Contact</NuxtLink></li>
+            <li><NuxtLink to="/profile"><font-awesome-icon icon="fa-solid fa-house" class="mr-2"/>Profile</NuxtLink></li>
         </ul>
     </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,10 +30,13 @@ export default ({
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     '@/assets/css/main.css',
+    '@fortawesome/fontawesome-svg-core/styles.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [
+    '~/plugins/fontawesome.ts'
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
