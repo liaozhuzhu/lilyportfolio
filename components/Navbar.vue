ddb57f24488808efcd974e3f4f8f11facb922dcb
Liao Zhu <liao.zhu.cs@gmail.com>
2023-07-04 13:20:55 -0500

    kajsdlkfj

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="navbar flex-col pl-5">
         <div>
-            <NuxtLink to="/"><img src="~/assets/imgs/LilyDosedel.svg" alt="logo" class="w-32 m-8 ml-0"/></NuxtLink>
+            <NuxtLink to="/"><img src="~/assets/imgs/LilyDosedel.svg" alt="logo" class="w-32 m-7 ml-0"/></NuxtLink>
         </div>
-        <ul class="flex-col leading-10 text-xl">
-            <li><NuxtLink to="/"><font-awesome-icon icon="fa-solid fa-house" class="mr-2"/>Home</NuxtLink></li>
-            <li><div><font-awesome-icon icon="fa-solid fa-search" class="mr-2"/>Search</div></li>
-            <li><NuxtLink to="/explore"><font-awesome-icon icon="fa-solid fa-compass" class="mr-2"/>Explore</NuxtLink></li>
-            <li><NuxtLink to="/reels"><font-awesome-icon icon="fa-solid fa-clapperboard" class="mr-2" />Reels</NuxtLink></li>
-            <li><NuxtLink to="/recommendations"><font-awesome-icon icon="fa-solid fa-comment-dots" class="mr-2"/>Recommendations</NuxtLink></li>
-            <li><NuxtLink to="/contact"><font-awesome-icon icon="fa-solid fa-square-plus" class="mr-2"/>Contact</NuxtLink></li>
-            <li><NuxtLink to="/profile"><font-awesome-icon icon="fa-solid fa-house" class="mr-2"/>Profile</NuxtLink></li>
+        <ul class="flex-col text-2xl">
+            <li class="navli"><NuxtLink to="/" class="flex justify-left items-center"><font-awesome-icon icon="fa-solid fa-house" class="mr-5"/><p class="text-base">Home</p></NuxtLink></li>
+            <li class="navli"><div class="flex justify-left items-center"><font-awesome-icon icon="fa-solid fa-search" class="mr-5"/><p class="text-base">Search</p></div></li>
+            <li class="navli"><NuxtLink to="/explore" class="flex justify-left items-center"><font-awesome-icon icon="fa-solid fa-compass" class="mr-5"/><p class="text-base">Explore</p></NuxtLink></li>
+            <li class="navli"><NuxtLink to="/reels" class="flex justify-left items-center"><font-awesome-icon icon="fa-solid fa-clapperboard" class="mr-5" /><p class="text-base">Reels</p></NuxtLink></li>
+            <li class="navli"><NuxtLink to="/recommendations" class="flex justify-left items-center"><font-awesome-icon icon="fa-solid fa-comment-dots" class="mr-5"/><p class="text-base">Messages</p></NuxtLink></li>
+            <li class="navli"><NuxtLink to="/contact" class="flex justify-left items-center"><font-awesome-icon icon="fa-solid fa-square-plus" class="mr-5"/><p class="text-base">Contact</p></NuxtLink></li>
+            <li class="navli"><NuxtLink to="/profile" class="flex justify-left items-center"><font-awesome-icon icon="fa-solid fa-house" class="mr-5"/><p class="text-base">Profile</p></NuxtLink></li>
         </ul>
     </div>
 </template>
@@ -21,5 +21,10 @@
 <style>
 .navbar {
     max-width: 300px;
+}
+
+.navli {
+    padding: 16px;
+    padding-left: 0;
 }
 </style>
