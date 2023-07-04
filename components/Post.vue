2de79f6080c6529912881a3a35840b4434f50f9f
Liao Zhu <liao.zhu.cs@gmail.com>
2023-07-04 09:11:43 -0500

    c

---
+++ components/Post.vue
@@ -0,0 +1,6 @@
+<template>
+    <h1>Post</h1>
+</template>
+
+<script setup lang="ts">
+</script>
---
+++ components/Suggested.vue
@@ -0,0 +1,6 @@
+<template>
+    <h1>Suggested</h1>
+</template>
+
+<script setup lang="ts">
+</script>
